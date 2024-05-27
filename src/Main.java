2e637a04d8dd65e114bb5a80eb680dbba3f8da00
Luis-Hernandezz <al02922128@tecmilenio.mx>
2024-05-27 11:21:13 -0600

    Practica 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[]   args) {
+
+        // 2 maneras de escribir algo, con variable o imprimir texto
+
+String x = "Hello world";
+
+        System.out.println(x);
+
+        System.out.printf("Hola mundo");
+
+
+    }
+}
